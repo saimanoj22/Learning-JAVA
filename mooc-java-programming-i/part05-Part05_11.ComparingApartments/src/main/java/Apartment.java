6dd96eebb7ef44e1d3c7333a9911733dfdf99728
Etcherla Sai Manoj <esmanoj.95@gmail.com>
2022-02-22 23:46:22 +0530

    Add Part05_11

--- mooc-java-programming-i/part05-Part05_11.ComparingApartments/src/main/java/Apartment.java
+++ mooc-java-programming-i/part05-Part05_11.ComparingApartments/src/main/java/Apartment.java
@@ -10,5 +10,39 @@ public class Apartment {
         this.squares = squares;
         this.princePerSquare = pricePerSquare;
     }
-
+    
+    public boolean largerThan(Apartment compared){
+        if(this.rooms > compared.rooms){
+            return true;
+        }
+        if(this.rooms < compared.rooms){
+            return false;
+        }
+        if(this.squares > compared.squares){
+            return true;
+        }
+        if(this.squares < compared.squares){
+            return false;
+        }
+        return false;
+    }
+    
+    public int priceDifference(Apartment compared){
+        if(this.squares*this.princePerSquare > compared.squares*compared.princePerSquare){
+            return (this.squares*this.princePerSquare) - (compared.squares*compared.princePerSquare);
+        }
+        else{
+            return (compared.squares*compared.princePerSquare) - (this.squares*this.princePerSquare);
+        }
+    }
+    
+    public boolean moreExpensiveThan(Apartment compared){
+        if(this.squares*this.princePerSquare > compared.squares*compared.princePerSquare){
+            return true;
+        }
+        if(this.squares*this.princePerSquare < compared.squares*compared.princePerSquare){
+            return false;
+        }
+        return false;
+    }
 }
