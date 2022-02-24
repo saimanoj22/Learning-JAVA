da5d3508d9be7d787480709bec84e35b5c721928
Etcherla Sai Manoj <esmanoj.95@gmail.com>
2022-02-24 14:49:04 +0530

    Add Part05 _15

--- mooc-java-programming-i/part05-Part05_15.Archive/src/main/java/Main.java
+++ mooc-java-programming-i/part05-Part05_15.Archive/src/main/java/Main.java
@@ -6,7 +6,28 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        ArrayList<Archive> archives = new ArrayList<>();
+        while(true){
+            System.out.println("Identifier? (empty will stop)");
+            String identifier = scanner.nextLine();
+            if(identifier.isEmpty()){
+                break;
+            }
+            System.out.println("Name? (empty will stop)");
+            String name = scanner.nextLine();
+            if(name.isEmpty()){
+                break;
+            }
+            Archive archive = new Archive(identifier, name);
+            if(!(archives.contains(archive))){
+                archives.add(archive);
+            }
+        }
+        
+        System.out.println("==Items==");
+        for(int i = 0; i < archives.size(); i++){
+            System.out.println(archives.get(i).getIdentifier() + ": " + archives.get(i).getName());
+        }
 
     }
 }
