0de0979c0d7fb2b72958b43a714d455b8719d684
Etcherla Sai Manoj <esmanoj.95@gmail.com>
2022-02-24 21:31:44 +0530

    Add Part05_16

--- mooc-java-programming-i/part05-Part05_16.DatingApp/src/main/java/SimpleDate.java
+++ mooc-java-programming-i/part05-Part05_16.DatingApp/src/main/java/SimpleDate.java
@@ -10,7 +10,75 @@ public class SimpleDate {
         this.month = month;
         this.year = year;
     }
+    
+    public void advance(){
+        if(this.day + 1 <= 30){
+            this.day++;
+        }
+        else{
+            if(this.month + 1 <= 12){
+                this.day = 1;
+                this.month++;
+            }
+            else{
+                this.day = 1;
+                this.month = 1;
+                this.year++;
+            }
 
+        }
+    }
+
+    public void advance(int howManyDays){
+        int yearsAdvance = howManyDays / 360;
+        this.year += yearsAdvance;
+        howManyDays -= (yearsAdvance * 360);
+        int monthAdvance = howManyDays / 30;
+        this.month += (howManyDays / 30);
+        
+        if(this.month > 12){
+            this.month -= 12;
+            this.year++;
+        }
+        howManyDays -= (monthAdvance * 30);
+        this.day += howManyDays;
+        if(this.day > 30){
+            this.day -= 30;
+            this.month++;
+        }
+        // code to be added so that it works for month = 1
+        if(this.month == 13){
+            this.month = 1;
+            this.year++;
+        }
+    }
+    
+    public SimpleDate afterNumberOfDays(int days){
+        SimpleDate clone = new SimpleDate(this.day, this.month, this.year);
+        int yearsAdvance = days / 360;
+        clone.year += yearsAdvance;
+        days -= (yearsAdvance * 360);
+        int monthAdvance = days / 30;
+        clone.month += (days / 30);
+        if(clone.month > 12){
+            clone.month -= 12;
+            clone.year++;
+        }
+        days -= (monthAdvance * 30);
+        clone.day += days;
+        if(clone.day > 30){
+            clone.day -= 30;
+            clone.month++;
+        }
+        // code to be added so that it works for month = 1
+        if(clone.month == 13){
+            clone.month = 1;
+            clone.year++;
+        }
+        
+        return clone;
+    }
+    
     @Override
     public String toString() {
         return this.day + "." + this.month + "." + this.year;
