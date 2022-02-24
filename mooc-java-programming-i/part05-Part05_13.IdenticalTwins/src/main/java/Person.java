5bd57a8e67dd0e0936ad431b2e82b7a628f6f22c
Etcherla Sai Manoj <esmanoj.95@gmail.com>
2022-02-24 12:40:14 +0530

    Add Part05_13

--- mooc-java-programming-i/part05-Part05_13.IdenticalTwins/src/main/java/Person.java
+++ mooc-java-programming-i/part05-Part05_13.IdenticalTwins/src/main/java/Person.java
@@ -16,4 +16,20 @@ public class Person {
     }
 
     // implement an equals method here for checking the equality of objects
+    public boolean equals(Object compared){
+        if(this == compared){
+            return true;
+        }
+        if(!(compared instanceof Person)){
+            return false;
+        }
+        Person comparedPerson = (Person) compared;
+        if(this.name.equals(comparedPerson.name) && 
+           this.birthday.equals(comparedPerson.birthday) &&
+           this.weight == comparedPerson.weight && 
+           this.height == comparedPerson.height){
+            return true;
+        }
+        return false;
+    }
 }
