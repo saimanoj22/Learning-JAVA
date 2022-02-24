0f7cc8b191b7511ed696e49dfd545d9417041bff
Etcherla Sai Manoj <esmanoj.95@gmail.com>
2022-02-24 22:02:28 +0530

    Add Part05_17

--- mooc-java-programming-i/part05-Part05_17.Money/src/main/java/Money.java
+++ mooc-java-programming-i/part05-Part05_17.Money/src/main/java/Money.java
@@ -22,6 +22,36 @@ public class Money {
     public int cents() {
         return this.cents;
     }
+    
+    public Money plus(Money addition){
+        int newEuros = this.euros + addition.euros;
+        int newCents = this.cents + addition.cents;
+        Money newMoney = new Money(newEuros, newCents);
+        return newMoney;
+    }
+    
+    public boolean lessThan(Money compared){
+        if(this.euros < compared.euros){
+            return true;
+        }
+        if(this.euros == compared.euros){ 
+            if (this.cents < compared.cents) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public Money minus(Money decreaser){
+        int thisCents = this.euros*100 + this.cents;
+        int decreaserCents = decreaser.euros*100 + decreaser.cents;
+        int result = thisCents - decreaserCents;
+        if(result < 0){
+            result = 0;
+        }
+        Money newMoney = new Money(0, result);
+        return newMoney;
+    }
 
     public String toString() {
         String zero = "";
