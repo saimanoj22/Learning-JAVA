b5ae78304aa52be8db8de8be45dc80caf041ceb8
Etcherla Sai Manoj <esmanoj.95@gmail.com>
2022-03-07 23:58:54 +0530

    Add Part 08_03

--- mooc-java-programming-ii/part08-Part08_03.LiquidContainers/src/main/java/LiquidContainers.java
+++ mooc-java-programming-ii/part08-Part08_03.LiquidContainers/src/main/java/LiquidContainers.java
@@ -5,17 +5,54 @@ public class LiquidContainers {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
+        int first = 0, second = 0;
 
         while (true) {
-            System.out.print("> ");
+            System.out.println("First: " + first + "/100");
+            System.out.println("Second: " + second + "/100");
 
             String input = scan.nextLine();
             if (input.equals("quit")) {
                 break;
             }
-
+            else{
+                String[] parts = input.split(" ");
+                String command = parts[0];
+                int value = Integer.valueOf(parts[1]);
+                if(command.equals("add")){
+                    if(value > 0){
+                        first += value;
+                        if(first > 100){
+                            first = 100;
+                        }
+                    }
+                }
+                else if(command.equals("move")){
+                    if(value > 0){
+                        if(first - value < 0){
+                            second += first;
+                        }
+                        else{
+                            second += value;
+                        }
+                        first -= value;
+                        if(first < 0){
+                            first = 0;
+                        }
+                        if(second > 100){
+                            second = 100;
+                        }
+                    }
+                }
+                else if(command.equals("remove")){
+                    if(value > 0){
+                        second -= value;
+                        if(second < 0){
+                            second = 0;
+                        }
+                    }
+                }
+            }
         }
     }
-
 }
