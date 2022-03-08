81657cfffa121d62a54a3e6a12e0c63c23e97cf9
Etcherla Sai Manoj <esmanoj.95@gmail.com>
2022-03-08 12:05:15 +0530

    Add Part08_04

---
+++ mooc-java-programming-ii/part08-Part08_04.LiquidContainers2/src/main/java/Container.java
@@ -0,0 +1,32 @@
+public class Container {
+    private int value;
+    public Container(){
+        this.value = 0;
+    }
+    
+    public int contains(){
+        return this.value;
+    }
+    
+    public void add(int amount){
+        if(amount > 0){
+            this.value += amount;
+            if(this.value > 100){
+                this.value = 100;
+            }
+        }
+    }
+    
+    public void remove(int amount){
+        if(amount > 0){
+            this.value -= amount;
+            if(this.value < 0){
+                this.value = 0;
+            }
+        }
+    }
+    
+    public String toString(){
+        return this.value + "/100";
+    }
+}
