5535bdef3aad8b06dc7a9f9f60ba546c032c427a
Etcherla Sai Manoj <esmanoj.95@gmail.com>
2022-04-21 05:24:54 +0530

    Add Chapter 10 The Comparable Interface

--- mooc-java-programming-ii/part10-Part10_13.LiteracyComparison/src/main/java/LiteracyComparison.java
+++ mooc-java-programming-ii/part10-Part10_13.LiteracyComparison/src/main/java/LiteracyComparison.java
@@ -3,10 +3,22 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 public class LiteracyComparison {
     
-    public static void main(String[] args) {
-
+    public static void main(String[] args){
+        try{
+            Files.lines(Paths.get("literacy.csv"))
+                    .map(row -> row.split(","))
+                    .sorted((a, b) -> a[5].compareTo(b[5]))
+                    .forEach(parts -> System.out.println(parts[3] + " (" + parts[4] + "), " + parts[2].split(" ")[1].trim() + ", " + parts[5]));
+        }
+        catch(IOException error){
+            System.out.println("Error: " + error.getMessage());
+        }
     }
 }
--- mooc-java-programming-ii/part10-Part10_14.Literature/src/main/java/MainProgram.java
+++ mooc-java-programming-ii/part10-Part10_14.Literature/src/main/java/MainProgram.java
@@ -1,11 +1,35 @@
 
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class MainProgram {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        ArrayList<Book> books = new ArrayList<>();
+        while(true){
+            System.out.println("Input the name of the book, empty stops: ");
+            String name = scanner.nextLine();
+            if(name.equals("")){
+                System.out.println(books.size() + " books in total.\n");
+                System.out.println("Books:");
+                break;
+            }
+            System.out.println("Input the age recommendation: ");
+            int age = Integer.valueOf(scanner.nextLine());
+            books.add(new Book(name, age));
+        }
+        
+        Comparator<Book> comparator = Comparator
+                .comparing(Book::getAge)
+                .thenComparing(Book::getName);
+        
+        Collections.sort(books, comparator);
+        for(Book b : books){
+            System.out.println(b);
+        }
     }
 
 }
