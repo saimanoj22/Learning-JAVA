54b1798f9a5e9284ea4e1fda2380c95da1ae62e0
Etcherla Sai Manoj <esmanoj.95@gmail.com>
2022-05-07 01:42:53 +0530

    Add Chapter 12 ArrayList and Hashtable

--- mooc-java-programming-ii/part12-Part12_04.List/src/main/java/Program.java
+++ mooc-java-programming-ii/part12-Part12_04.List/src/main/java/Program.java
@@ -1,4 +1,5 @@
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Program {
@@ -6,7 +7,53 @@ public class Program {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         // test your class here
+        ArrayList<String> strings = new ArrayList<>();
+        System.out.println(strings.contains("Hello!"));
+        strings.add("Hello!");
+        System.out.println(strings.contains("Hello!"));
+        strings.remove("Hello!");
+        System.out.println(strings.contains("Hello!"));
+        System.out.println("");
 
+        List<String> myList = new List<>();
+        myList.add("hello");
+        myList.add("world");
+        System.out.println("");
+
+        myList = new List<>();
+        System.out.println(myList.contains("hello"));
+        myList.add("hello");
+        System.out.println(myList.contains("hello"));
+        System.out.println("");
+
+        myList = new List<>();
+        System.out.println(myList.contains("hello"));
+        myList.add("hello");
+        System.out.println(myList.contains("hello"));
+        myList.remove("hello");
+        System.out.println(myList.contains("hello"));
+        System.out.println("");
+
+        myList = new List<>();
+        System.out.println(myList.contains("hello"));
+        myList.add("hello");
+        System.out.println(myList.contains("hello"));
+        int index = myList.indexOfValue("hello");
+        System.out.println(index);
+        System.out.println(myList.value(index));
+        myList.remove("hello");
+        System.out.println(myList.contains("hello"));
+        System.out.println("");
+
+        myList = new List<>();
+        myList.add("hello");
+        myList.add("world");
+        System.out.println("");
+
+        for (int i = 0; i < myList.size(); i++) {
+            System.out.println(myList.value(i));
+        }
+        System.out.println("");
     }
 
 }
--- mooc-java-programming-ii/part12-Part12_05.HashMap/src/main/java/Program.java
+++ mooc-java-programming-ii/part12-Part12_05.HashMap/src/main/java/Program.java
@@ -6,7 +6,10 @@ public class Program {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // You can test the class here
-
+        HashMap<String, Integer> hashmap = new HashMap<>();
+        hashmap.add("one", 1);
+        System.out.println(hashmap.get("one"));
+        System.out.println(hashmap.get("two"));
     }
 
 }
