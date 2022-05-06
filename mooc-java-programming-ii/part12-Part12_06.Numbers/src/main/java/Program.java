05266074af937aa2e1f8eb1607186c7ec478e6e5
Etcherla Sai Manoj <esmanoj.95@gmail.com>
2022-05-07 02:09:39 +0530

    Add Chapter 12 Randomness

--- mooc-java-programming-ii/part12-Part12_06.Numbers/src/main/java/Program.java
+++ mooc-java-programming-ii/part12-Part12_06.Numbers/src/main/java/Program.java
@@ -6,6 +6,12 @@ public class Program {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Random numbers = new Random();
+        System.out.println("How many random numbers should be printed?");
+        int num = Integer.valueOf(scanner.nextLine());
+        for(int i = 0; i < num ; i++){
+            System.out.println(numbers.nextInt(11));
+        }
     }
 
 }
--- mooc-java-programming-ii/part12-Part12_08.Lottery/src/main/java/LotteryRow.java
+++ mooc-java-programming-ii/part12-Part12_08.Lottery/src/main/java/LotteryRow.java
@@ -5,6 +5,7 @@ import java.util.Random;
 public class LotteryRow {
 
     private ArrayList<Integer> numbers;
+    private Random random;
 
     public LotteryRow() {
         // Draw the numbers when the LotteryRow is created
@@ -18,13 +19,22 @@ public class LotteryRow {
     public void randomizeNumbers() {
         // Initialize the list for numbers
         this.numbers = new ArrayList<>();
+        this.random = new Random();
         // Implement the random number generation here
         // the method containsNumber is probably useful
+        while(this.numbers.size() < 7){
+            int picked = this.random.nextInt(40) + 1;
+            if (!containsNumber(picked)) {
+                this.numbers.add(picked);
+            }
+        }
     }
 
     public boolean containsNumber(int number) {
         // Check here whether the number is among the drawn numbers
+        if (this.numbers.contains(number)) {
+            return true;
+        }
         return false;
     }
 }
-
