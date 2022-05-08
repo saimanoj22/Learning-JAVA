57727ac1d7f431fa6385b7d5101866570e87d792
Etcherla Sai Manoj <esmanoj.95@gmail.com>
2022-05-08 13:57:33 +0530

    Add Chapter 13 Event Handling

--- mooc-java-programming-ii/part13-Part13_06.Notifier/src/main/java/notifier/NotifierApplication.java
+++ mooc-java-programming-ii/part13-Part13_06.Notifier/src/main/java/notifier/NotifierApplication.java
@@ -1,11 +1,39 @@
 package notifier;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-public class NotifierApplication {
+public class NotifierApplication extends Application{
 
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        launch(NotifierApplication.class);
+    }
+
+    @Override
+    public void start(Stage window) {
+        VBox contents = new VBox();
+        TextField text = new TextField();
+        Button button = new Button("Update");
+        Label label = new Label();
+        
+        button.setOnAction((event) ->{
+            label.setText(text.getText());
+        });
+        
+        contents.getChildren().add(text);
+        contents.getChildren().add(button);
+        contents.getChildren().add(label);
+        
+        Scene view = new Scene(contents);
+        window.setScene(view);
+        window.show();
     }
 
 }
--- mooc-java-programming-ii/part13-Part13_07.TextStatisticsPart2/src/main/java/textstatistics/TextStatisticsApplication.java
+++ mooc-java-programming-ii/part13-Part13_07.TextStatisticsPart2/src/main/java/textstatistics/TextStatisticsApplication.java
@@ -1,11 +1,57 @@
 package textstatistics;
 
+import java.util.Arrays;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
-public class TextStatisticsApplication {
-
+public class TextStatisticsApplication extends Application {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        launch(TextStatisticsApplication.class);
+    }
+
+    @Override
+    public void start(Stage window) {
+        BorderPane layout = new BorderPane();
+        HBox text = new HBox();
+        Label letters = new Label("Letters: 0");
+        Label words = new Label("Words: 0");
+        Label longest = new Label("The longest word is:");
+
+        text.setSpacing(10);
+        text.getChildren().add(letters);
+        text.getChildren().add(words);
+        text.getChildren().add(longest);
+
+        TextArea area = new TextArea();
+        layout.setCenter(area);
+        layout.setBottom(text);
+
+        area.textProperty().addListener((change, oldValue, newValue) -> {
+            int characters = newValue.length();
+            String[] parts = newValue.split(" ");
+            int noOfWords = parts.length;
+            String longestString = Arrays.stream(parts)
+                    .sorted((s1, s2) -> s2.length() - s1.length())
+                    .findFirst()
+                    .get();
+            
+            letters.setText("Letters: " + characters);
+            words.setText("Words: " + noOfWords);
+            longest.setText("The longest word is: " + longestString);
+        });
+
+        Scene view = new Scene(layout);
+
+        window.setScene(view);
+        window.show();
     }
 
 }
