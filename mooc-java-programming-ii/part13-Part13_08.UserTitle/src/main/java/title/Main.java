15a7d2dd1883538030a2d49399281bbff162c730
Etcherla Sai Manoj <esmanoj.95@gmail.com>
2022-05-08 15:40:30 +0530

    Add Chapter 13 Application launch parameters

--- mooc-java-programming-ii/part13-Part13_08.UserTitle/src/main/java/title/Main.java
+++ mooc-java-programming-ii/part13-Part13_08.UserTitle/src/main/java/title/Main.java
@@ -1,11 +1,16 @@
 package title;
 
+import java.util.Scanner;
+import javafx.application.Application;
 
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         System.out.println("Hello world!");
-
+        System.out.println("Enter Tilte of Window : ");
+        String title = scan.nextLine();
+        Application.launch(UserTitle.class, "--title=" + title);
     }
 
 }
